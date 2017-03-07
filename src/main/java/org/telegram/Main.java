2f153ef61f8dc7144e5f60a12d2fa1817eda2843
sushiomsky <harrybotya@gmail.com>
2017-03-07 23:44:57 +0100

    branch init

--- src/main/java/org/telegram/Main.java
+++ src/main/java/org/telegram/Main.java
@@ -41,12 +41,13 @@ public class Main {
             TelegramBotsApi telegramBotsApi = createTelegramBotsApi();
             try {
                 // Register long polling bots. They work regardless type of TelegramBotsApi we are creating
-                telegramBotsApi.registerBot(new ChannelHandlers());
+            /*    telegramBotsApi.registerBot(new ChannelHandlers());
                 telegramBotsApi.registerBot(new DirectionsHandlers());
                 telegramBotsApi.registerBot(new RaeHandlers());
                 telegramBotsApi.registerBot(new WeatherHandlers());
                 telegramBotsApi.registerBot(new TransifexHandlers());
                 telegramBotsApi.registerBot(new FilesHandlers());
+                */
                 telegramBotsApi.registerBot(new CommandsHandler());
             } catch (TelegramApiException e) {
                 BotLogger.error(LOGTAG, e);
